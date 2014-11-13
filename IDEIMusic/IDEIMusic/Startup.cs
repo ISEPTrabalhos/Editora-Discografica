05733159b75bffb251c3db0464cbc46fefcc4e4d
António Silva <antoniosilva1017@gmail.com>
2014-11-13 20:59:01 +0000

    Initial project commit

---
+++ IDEIMusic/IDEIMusic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IDEIMusic.Startup))]
+namespace IDEIMusic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
