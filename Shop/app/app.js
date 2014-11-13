5063158ce76a9dc3e38604d4cd04da4522059be9
Mario Garrido <mrfhitz@gmail.com>
2014-11-13 15:53:27 +0000

    Add the new design to the AngularJS app

---
+++ Shop/app/app.js
@@ -0,0 +1,18 @@
+var app = angular.module('app', [
+			'ui.router'
+		]);
+
+app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+	$urlRouterProvider.otherwise('/error');
+
+	$stateProvider
+		.state('home', {
+			url: '/',
+			templateUrl: 'views/cds_view.html',
+			controller: 'home_controller'
+		})
+		.state('error', {
+			url: '/error',
+			templateUrl: 'views/error_view.html'
+		})
+}]);
---
+++ Shop/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shop</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<meta charset="utf-8">
+</head>
+<body>
+
+<div class="main-app" ng-app="app">
+	<header ng-include="'views/header.html'"></header>
+	<div ui-view></div>
+	<footer ng-include="'views/footer.html'"></footer>
+</div>
+
+
+<script type="text/javascript" src="assets/js/jquery.js"></script>
+<script type="text/javascript" src="assets/js/angular.min.js"></script>
+<script type="text/javascript" src="assets/js/angular-ui-router.min.js"></script>
+
+<script type="text/javascript" src="app/app.js"></script>
+<script type="text/javascript" src="controllers/home_controller.js"></script>
+</body>
+</html>
