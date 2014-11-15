f84c8283a4419d278e40da4dea5b8118211b2cba
Paulo Oliveira <pajolool1471@gmail.com>
2014-11-15 02:26:22 +0000

    Shopping Cart file create ref #7

---
+++ Shop/app/ShoppingCart.js
@@ -0,0 +1,16 @@
+/*
+	Shopping Cart Methods
+*/
+
+
+//load from session or create the shopping cart 
+function createShoppingCart()  {
+}
+
+//add new cd product to shopping cart
+function addCDtoCart() {
+}
+
+// remove cd from shopping cart
+function removeCDfromCart() {
+}
