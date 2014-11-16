b95960b57ea8f7a789c7b430e18a6ca1e94482e3
Paulo Oliveira <pajolool1471@gmail.com>
2014-11-16 00:39:22 +0000

    Add product to cart done ref #7

--- Shop/app/ShoppingCart.js
+++ Shop/app/ShoppingCart.js
@@ -2,19 +2,7 @@
 	Shopping Cart Methods
 */
 
-//load shopping cart 'session' 
-function loadShoppingCart()  {
-	if(isSet()==false) {
-		console.log("Não existe carrinho !!");
-		return false;
-	}
-	console.log('Existe carrinho !!');
-	// update shopping cart info on header
-	updateCartInfo();
-	return true;
-}
-
-//
+//check if cart exists
 function isSet() {
 	if(localStorage.getItem('cart')==null) {
 		return false;
@@ -22,52 +10,41 @@ function isSet() {
 	return true;
 }
 
-//get products list
-function getProducstFromShoppingCart() {
-	// no need to check if cart exist, done earlier
-	var json = JSON.parse(localStorage.getItem('cart'));
-	return json.products;
+//load shopping cart 'session' 
+function loadShoppingCart()  {
+	if(isSet()==false) {
+		return false;
+	}
+	// update shopping cart info on header
+	updateCartInfo();
+	return true;
+}
+
+function getCart() {
+	return localStorage.getItem('cart');
 }
 
 //create shopping cart 'session'
 function createShoppingCart(products) {
-	window.localStorage.setItem("cart",JSON.stringify(products));
+	if(isSet()) { // if cart exists remove the old one
+		localStorage.removeItem("cart");
+	}
+	window.localStorage.setItem("cart",products);
 	updateCartInfo();
 }
 
 //add new cd product to shopping cart
 function addCDtoCart(id) {
 	console.log("Add to cart " + id);
-	/* simular conjunto de produtos
-	var products = '{ "products" : [' +
-		'{ "id":' + id +' , "price":"17.99" },' +
-		'{ "id":"2" , "price":"7.30" },' +
-		'{ "id":"3" , "price":"5.99" } ]}';
-		*/
-	if(isSet() == false) { // se nao exister criar uma nova
-		
-		var products = '{ "products" : [' +
-		'{ "id":' + id +' , "price":"19.99" }' + ']}';
-		
-		createShoppingCart(JSON.parse(products));
+	if(isSet() == false) { // se nao exister carrinho
+		var products = [id];
+		createShoppingCart(products);
 	} else { // se existir adicionar
-		console.log(JSON.parse(localStorage.getItem('cart')));
-		// adicionar ao array
-		// ..
-		//update cart header info
-		updateCartInfo();
+		var products_string = getCart();
+		var products = products_string.split(',');
+		products.push(id);
+		createShoppingCart(products);
 	}
-	/*
-		var json_products = JSON.parse(localStorage.getItem('cart'));
-		var newProduct = '{"id":"1777", "price":"19.99"}';
-		console.log("asdasdas");
-		//console.log(json_products);
-		console.log(newProduct);
-
-		//json_products += newProduct;
-		//simulate album product
-		
-		*/
 }
 
 // remove cd from shopping cart
@@ -77,8 +54,13 @@ function removeCDfromCart() {
 // update shopping cart info on header
 function updateCartInfo() {
 	var cart_label = document.getElementById("cart_label");
-	var json = JSON.parse(localStorage.getItem('cart'));
-	var products = getProducstFromShoppingCart();
-	cart_label.innerHTML = "(" + products.length + ")"; // size of current shopping cart
-	cart_label.style.display = "inline-block";
+	if(isSet()==true) {
+		console.log('sim');
+		var cart = getCart();
+		var items = cart.split(',').length;
+		cart_label.innerHTML = "(" + items + ")"; // number of items of current shopping cart
+	} else if(isSet()==false) {
+		console.log('nao');
+		cart_label.innerHTML = "(0)";
+	}
 }
