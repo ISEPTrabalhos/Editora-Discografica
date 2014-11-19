87421f022b403d274a99f0a9a96eb96a50badb59
Mario Garrido <mrfhitz@gmail.com>
2014-11-19 22:21:49 +0000

    Just a warning to the databaseconfig

--- Shop/assets/php/connection.php
+++ Shop/assets/php/connection.php
@@ -3,3 +3,9 @@
 	define(DB_USERNAME, "");
 	define(DB_PASSWORD, "");
 	define(DB_DATABASE, "");
+	
+/**
+ * if change pls use the following command:
+ * git update-index --assume-unchanged assets/php/connection.php
+ * (You need to be in the Shop folder, if you are not pls change the path)
+ */
