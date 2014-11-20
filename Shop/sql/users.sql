4c0ec2841c1b399a235ce9d95f62cc0e37a32b2f
Mario Garrido <mrfhitz@gmail.com>
2014-11-20 15:08:26 +0000

    Just remove all the bad comments

--- Shop/sql/users.sql
+++ Shop/sql/users.sql
@@ -1,30 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.2.8
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Nov 19, 2014 at 10:49 PM
--- Server version: 5.6.20
--- PHP Version: 5.5.14
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `myshop`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE IF NOT EXISTS `users` (
 `id` int(11) NOT NULL,
@@ -35,34 +10,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `users`
---
-
 INSERT INTO `users` (`id`, `username`, `password`, `name`, `email`, `timestamp`) VALUES
 (1, 'mrfhitz', '4297f44b13955235245b2497399d7a93', 'Mário', 'mrfhitz@gmail.com', '2014-11-19 22:32:29');
 
 INSERT INTO `users` (`id`, `username`, `password`, `name`, `email`, `timestamp`) VALUES
 (2, 'pajo1471', '32250170a0dca92d53ec9624f336ca24', 'Paulo', 'pajolool1471@gmail.com', '2014-11-20 10:46:00');
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
  ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
