901d0f6b81d588944822804c9f796519fee6d46a
António Silva <antoniosilva1017@gmail.com>
2014-11-22 22:33:29 +0000

    Added the .NET Web Service Skeleton #41

---
+++ IDEIMusic/IDEIMusic/Services/IIDEIMusicService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using IDEIMusic.Models;
+
+namespace IDEIMusic.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IIDEIMusicService" in both code and config file together.
+    [ServiceContract]
+    public interface IIDEIMusicService
+    {
+        [OperationContract]
+        List<Album> getCatalog();
+    }
+}
