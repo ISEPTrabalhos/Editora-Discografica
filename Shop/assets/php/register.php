a9138c12a0c33dcb797ee1973e4b4ecbdba20404
Paulo Oliveira <pajolool1471@gmail.com>
2014-11-25 21:32:25 +0000

    Register system - username & email checked automatically close #49

--- Shop/assets/php/register.php
+++ Shop/assets/php/register.php
@@ -2,33 +2,71 @@
 error_reporting(0);
 require 'connection.php';
 
-// DB_HOSTNAME DB_USERNAME DB_PASSWORD DB_DATABASE
+$function = $_GET['func'];
 
-$row = array();
-$error = false;
-if(isset($_GET['username']) && isset($_GET['password'])
-	&& isset($_GET['email']) && isset($_GET['name'])) {
-	$username = trim($_GET['username']);
-	$password = trim($_GET['password']);
-	$email = trim($_GET['email']);
-	$name = trim($_GET['name']);
+switch ($function) {
+	case 'checkUsername':
+		echo checkUsername();
+		break;
+	case 'checkEmail':
+		echo checkEmail();
+		break;
+	case 'registerUser':
+		echo registerUser();
+		break;
+}
 
-	if(!empty($username) && !empty($password) && !empty($email)) {
-		global $db;
+function checkUsername() {
+	global $db;
+	if(isset($_GET['username'])) {
+		$username = trim($_GET['username']);
+		if(!empty($username)) {
+			$statement = $db->prepare("SELECT * FROM users WHERE username = :username");
+			$statement->execute(array(':username' => $username));
+			$row = $statement->fetchAll(PDO::FETCH_ASSOC);
+			if($row) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
 
-		$statement = $db->prepare("SELECT * FROM users WHERE username = :username OR email = :email");
-		$statement->execute(array(':username' => $username, ':email' => $email));
-		$row = $statement->fetchAll(PDO::FETCH_ASSOC);
-		if($row) {
-			$error = 'Username or email already in use';
-		} else {
-			// insert user 
+function checkEmail() {
+	global $db;
+	if(isset($_GET['email'])) {
+		$email = trim($_GET['email']);
+		if(!empty($email)) {
+			$statement = $db->prepare("SELECT * FROM users WHERE email = :email");
+			$statement->execute(array(':email' => $email));
+			$row = $statement->fetchAll(PDO::FETCH_ASSOC);
+			if($row) {
+				return false;
+			}
+		}
+	}
+	//echo 'TRUE';
+	return true;
+}
+
+function registerUser() {
+	$row = array();
+	$error = false;
+	if(isset($_GET['username']) && isset($_GET['password'])
+		&& isset($_GET['email']) && isset($_GET['name'])) {
+		$username = trim($_GET['username']);
+		$password = trim($_GET['password']);
+		$email = trim($_GET['email']);
+		$name = trim($_GET['name']);
+
+		if(!empty($username) && !empty($password) && !empty($email)) {
+			global $db;
 			$statement = $db->prepare("INSERT INTO users (username,password,name,email) VALUES(:username,:password,:name,:email)");
 			$statement->execute(array(':username' => $username, ':password' => md5($password), ':email' => $email, ':name' => $name));	
 			$id = $db->lastInsertId();
-		}
-		
+		} else $error = 'There are invalid fields';
 	} else $error = 'There are invalid fields';
-} else $error = 'There are invalid fields';
 
-echo json_encode(array('user' => $id, 'error' => $error));
+	echo json_encode(array('user' => $id, 'error' => $error));
+
+}
--- Shop/controllers/register_controller.js
+++ Shop/controllers/register_controller.js
@@ -17,13 +17,33 @@ angular
 		$scope.username_error = "";
 
 		$scope.checkEmail = function() {
-			alert("Check Email");
-			$scope.email_error = "Email already in use";
+			if($scope.email !== undefined && $scope.email.trim().length !== 0) {
+				$http.get("assets/php/register.php?func=checkEmail&email="+$scope.email)
+				.success(function(data) {
+					if(data == false) {
+						$scope.email_error = "Email already in use.";
+					} else {
+						$scope.email_error = "Email available.";
+					}
+				});
+			} else {
+				$scope.email_error = "";
+			}
 		}
 
 		$scope.checkUsername = function() {
-			alert("Check Username");
-			$scope.username_error = "Username already in use";
+			if($scope.username !== undefined && $scope.username.trim().length !== 0) {
+				$http.get("assets/php/register.php?func=checkUsername&username="+$scope.username)
+				.success(function(data) {
+					if(data == false) {
+						$scope.username_error = "Username already in use.";
+					} else {
+						$scope.username_error = "Username available.";
+					}
+				});
+			} else {
+				$scope.username_error = "";
+			}
 		}
 
 
@@ -36,12 +56,12 @@ angular
 					&& $scope.name.trim().length!== 0)
 				{
 					if($scope.password === $scope.newpassword) {
-						$http.get("assets/php/register.php?username="+$scope.username+"&password="+$scope.password+"&email="+$scope.email+"&name="+$scope.name)
+						$http.get("assets/php/register.php?func=registerUser&username="+$scope.username+"&password="+$scope.password+"&email="+$scope.email+"&name="+$scope.name)
 						.success(function(data) {
 							if(data.error == false){
 								// sign in user
 								window.localStorage.setItem("userid", data.user);
-								window.localStorage.setItem("message", "You're now logged in.");
+								window.localStorage.setItem("message", "Thank you for sign up. You're now automatically logged in.");
 								$location.path('/');
 								$location.replace();
 							}else $scope.error = data;
