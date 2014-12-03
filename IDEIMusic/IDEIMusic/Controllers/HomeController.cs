98b464b31295577c346208764eef5c475bad6e48
António Silva <antoniosilva1017@gmail.com>
2014-12-03 16:25:11 +0000

    Rework on page design and added roles close #63

--- IDEIMusic/IDEIMusic/Controllers/HomeController.cs
+++ IDEIMusic/IDEIMusic/Controllers/HomeController.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IDEIMusic.DAL;
 
 namespace IDEIMusic.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private IDEIMusicContext db = new IDEIMusicContext();
+
+        public ActionResult Index(string sortOrder, string searchString)
         {
-            return View();
+            ViewBag.TitleSortParm = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            ViewBag.PriceSortParm = sortOrder == "Price" ? "price_desc" : "Price";
+
+            var albums = from a in db.Albums
+                         select a;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                albums = albums.Where(a => a.title.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    albums = albums.OrderByDescending(a => a.title);
+                    break;
+                case "Price":
+                    albums = albums.OrderBy(a => a.price);
+                    break;
+                case "price_desc":
+                    albums = albums.OrderByDescending(a => a.price);
+                    break;
+                default:
+                    albums = albums.OrderBy(a => a.title);
+                    break;
+            }
+            return View(albums.ToList());
         }
 
         public ActionResult About()
