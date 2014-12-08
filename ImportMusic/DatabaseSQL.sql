3ff46f9fcbb369eca4e50178ed867a245186882e
António Silva <antoniosilva1017@gmail.com>
2014-12-08 00:35:41 +0000

    SQL UPDATE

--- ImportMusic/DatabaseSQL.sql
+++ ImportMusic/DatabaseSQL.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Dec 06, 2014 at 01:40 AM
+-- Generation Time: Dec 08, 2014 at 01:35 AM
 -- Server version: 5.5.38
 -- PHP Version: 5.5.14
 
@@ -24,18 +24,15 @@ CREATE TABLE `Sale` (
 `ID` int(11) NOT NULL,
   `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `Total` decimal(10,0) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `Sale`
 --
 
 INSERT INTO `Sale` (`ID`, `Date`, `Total`) VALUES
-(1, '2014-12-06 00:35:34', 0),
-(2, '2014-12-06 00:36:50', 0),
-(3, '2014-12-06 00:37:46', 100),
-(4, '2014-12-06 00:38:41', 0),
-(5, '2014-12-06 00:39:15', 40);
+(1, '2014-12-08 00:29:00', 40),
+(2, '2014-12-08 00:32:27', 40);
 
 -- --------------------------------------------------------
 
@@ -48,16 +45,19 @@ CREATE TABLE `SaleDetails` (
   `SaleID` int(11) NOT NULL,
   `Album` varchar(128) NOT NULL,
   `Quantity` int(11) NOT NULL,
-  `Price` decimal(10,0) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `Price` decimal(10,0) NOT NULL,
+  `Type` varchar(128) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `SaleDetails`
 --
 
-INSERT INTO `SaleDetails` (`ID`, `SaleID`, `Album`, `Quantity`, `Price`) VALUES
-(1, 5, 'yes', 2, 10),
-(2, 5, 'no', 2, 10);
+INSERT INTO `SaleDetails` (`ID`, `SaleID`, `Album`, `Quantity`, `Price`, `Type`) VALUES
+(1, 1, '1989', 1, 20, 'CD'),
+(2, 1, 'Cadillactica', 1, 20, 'CD'),
+(3, 2, '1989', 1, 20, 'CD'),
+(4, 2, 'Cadillactica', 1, 20, 'CD');
 
 --
 -- Indexes for dumped tables
@@ -83,9 +83,9 @@ ALTER TABLE `SaleDetails`
 -- AUTO_INCREMENT for table `Sale`
 --
 ALTER TABLE `Sale`
-MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `SaleDetails`
 --
 ALTER TABLE `SaleDetails`
-MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
